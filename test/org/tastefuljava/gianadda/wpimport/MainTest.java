75efc666c92279083f8d92a4b4617515bdd7b455
Maurice Perry <maurice@perry.ch>
2014-11-16 20:15:18 +0100

    Initial commit

--- test/org/tastefuljava/gianadda/wpimport/MainTest.java
+++ test/org/tastefuljava/gianadda/wpimport/MainTest.java
@@ -8,22 +8,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MainTest {
-    
     public MainTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
