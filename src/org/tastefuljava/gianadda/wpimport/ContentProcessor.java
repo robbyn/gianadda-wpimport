ce7ade706895af36fe8f2527d12bba3bd8c83bbb
Maurice Perry <maurice.perry@etat.ge.ch>
2014-11-20 11:16:46 +0100

    Initial commit

--- src/org/tastefuljava/gianadda/wpimport/ContentProcessor.java
+++ src/org/tastefuljava/gianadda/wpimport/ContentProcessor.java
@@ -1,5 +1,10 @@
 package org.tastefuljava.gianadda.wpimport;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
 public class ContentProcessor {
     private final String baseUrl;
 
@@ -7,10 +12,11 @@ public class ContentProcessor {
         this.baseUrl = baseUrl;
     }
 
-    public String process(String input) {
+    public String process(String input) throws IOException {
         String body = input;
         body = cleanup(body, "[caption", "[/caption]");
         body = cleanup(body, "<iframe", "</iframe>");
+        body = splitParas(body);
         return body;
     }
 
@@ -32,6 +38,33 @@ public class ContentProcessor {
         }
         if (start < content.length()) {
             buf.append(content.substring(start));
+        }
+        return buf.toString();
+    }
+
+    private String splitParas(String body) throws IOException {
+        StringBuilder buf = new StringBuilder();
+        try (Reader reader = new StringReader(body);
+                BufferedReader in = new BufferedReader(reader)) {
+            int paraStart = 0;
+            for (String s = in.readLine(); s != null; s = in.readLine()) {
+                if (s.trim().length() == 0) {
+                    if (buf.length() > paraStart) {
+                        buf.insert(paraStart, "<p>");
+                        buf.append("</p>\n");
+                        paraStart = buf.length();
+                    }
+                } else {
+                    if (buf.length() > paraStart) {
+                        buf.append('\n');
+                    }
+                    buf.append(s);
+                }
+            }
+            if (buf.length() > paraStart) {
+                buf.insert(paraStart, "<p>");
+                buf.append("</p>");
+            }
         }
         return buf.toString();
     }
